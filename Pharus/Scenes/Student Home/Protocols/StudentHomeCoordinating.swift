ae2d6bc387e40e8113fe1bf4fbcf0812a300f452
VIctor <colen.victor@icloud.com>
2022-06-17 10:25:24 -0300

    refactor: rename Student Home coordinator protocol

--- Pharus/Scenes/Student Home/Protocols/StudentHomeCoordinating.swift
+++ Pharus/Scenes/Student Home/Protocols/StudentHomeCoordinating.swift
@@ -1,5 +1,5 @@
 //
-//  StudentHomeFlow.swift
+//  StudentHomeCoordinating.swift
 //  Pharus
 //
 //  Created by Victor Colen on 17/06/22.
@@ -7,4 +7,4 @@
 
 import Foundation
 
-protocol StudentHomeFlow { }
+protocol StudentHomeCoordinating { }
