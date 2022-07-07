d603d572cf43af50f79c4cdac9c69f080b579dd1
VIctor <colen.victor@icloud.com>
2022-07-07 13:40:53 -0300

    refactor: create KeyPathSettable protocol

---
+++ Modules/PharusUI/Sources/PharusUI/Protocols/KeyPathSettable.swift
@@ -0,0 +1,34 @@
+//
+//  KeyPathSettable.swift
+//
+//
+//  Created by Victor Colen on 07/07/22.
+//
+
+import Foundation
+
+public protocol KeyPathSettable { }
+
+public extension KeyPathSettable {
+     func setting<Value>(
+        _ keyPath: WritableKeyPath<Self, Value>,
+        to value: Value
+    ) -> Self {
+
+        var newSelf = self
+        newSelf[keyPath: keyPath] = value
+        return newSelf
+    }
+
+    func setup(_ setup: (inout Self) -> Void) -> Self {
+        var newSelf = self
+        setup(&newSelf)
+        return newSelf
+    }
+
+    func assign(to reference: inout Self?) -> Self {
+        guard var reference = reference else { return self }
+        reference = self
+        return reference
+    }
+}
