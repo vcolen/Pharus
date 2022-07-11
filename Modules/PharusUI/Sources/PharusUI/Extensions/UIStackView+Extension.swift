91f169346f49209dc2bf2508d84522dd7c55eecf
VIctor <colen.victor@icloud.com>
2022-07-11 09:19:19 -0300

    refactor: add StackView Extension

--- Modules/PharusUI/Sources/PharusUI/Extensions/UIStackView+Extension.swift
+++ Modules/PharusUI/Sources/PharusUI/Extensions/UIStackView+Extension.swift
@@ -8,6 +8,13 @@ import UIKit
 
 public extension UIStackView {
 
+    convenience init(_ views: [UIView]) {
+        self.init()
+        for view in views {
+            addArrangedSubview(view)
+        }
+    }
+
     func removeFully(view: UIView) {
         removeArrangedSubview(view)
         view.removeFromSuperview()
