30b62992ef66abc1265356d11734ebecbb8c7ae2
VIctor <colen.victor@icloud.com>
2022-07-11 11:38:17 -0300

    refactor: add new stack view initializer

--- Modules/PharusUI/Sources/PharusUI/Extensions/UIStackView+Extension.swift
+++ Modules/PharusUI/Sources/PharusUI/Extensions/UIStackView+Extension.swift
@@ -9,7 +9,7 @@ import UIKit
 public extension UIStackView {
 
     convenience init(_ views: [UIView]) {
-        self.init()
+        self.init(frame: .zero)
         for view in views {
             addArrangedSubview(view)
         }
