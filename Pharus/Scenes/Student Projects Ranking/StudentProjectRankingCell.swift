2ed0e1fec48232bc31785bccc2966fd268465a19
VIctor <colen.victor@icloud.com>
2022-07-28 09:51:34 -0300

    refactor: StudentProjectsRankingCell in declarative view code

--- Pharus/Scenes/Student Projects Ranking/StudentProjectRankingCell.swift
+++ Pharus/Scenes/Student Projects Ranking/StudentProjectRankingCell.swift
@@ -4,23 +4,36 @@
 //
 //  Created by Victor Colen on 04/04/22.
 //
+
 import UIKit
 import PharusUI
 
 class StudentProjectRankingCell: UITableViewCell {
 
+    // MARK: - Views
     private lazy var mainStackView = VStackView([
         projectTitleLabel,
         congratulationsLabel,
         placementStackView,
-        mentorCommentsStackView
+
+        VStackView([
+            VStackView([
+                mentorCommentsTitleLabel
+            ])
+            .frame(height: 54),
+
+            VStackView([
+                mentorCommentsDescriptionLabel
+            ])
+            .frame(height: 80)
+        ])
     ])
-        .setting(\.spacing, to: 32)
-        .padding([.top, .leading, .trailing, .bottom], 16)
-        .setting(\.backgroundColor, to: .Purple.pharusPurple)
-        .setting(\.layer.cornerRadius, to: 16)
-        .padding([.top], 24.5)
-        .padding([.leading, .bottom, .trailing], 8)
+    .setting(\.spacing, to: 32)
+    .padding([.top, .leading, .trailing, .bottom], 16)
+    .setting(\.backgroundColor, to: .Purple.pharusPurple)
+    .setting(\.layer.cornerRadius, to: 16)
+    .padding([.top], 24.5)
+    .padding([.leading, .bottom, .trailing], 8)
 
     private lazy var projectTitleLabel = UILabel()
         .setting(\.textAlignment, to: .center)
@@ -35,97 +48,64 @@ class StudentProjectRankingCell: UITableViewCell {
         .setting(\.font, to: .mediumTitleSemiBold)
 
     private lazy var placementStackView = HStackView([
-        placementHelperView,
-        medalHelperView
+        placementImageView,
+        medalImageView
     ])
-        .setting(\.distribution, to: .fillEqually)
-        .frame(height: 133)
-
-    private lazy var placementHelperView = UIView()
+        .setting(\.spacing, to: 30)
+        .frame(height: 103)
 
     private lazy var placementImageView = UIImageView()
         .setting(\.image, to: .pharusImages.thirdPlaceImage)
 
     private lazy var medalImageView = UIImageView()
         .setting(\.image, to: .pharusImages.thirdPlaceMedalImage)
+        .frame(width: 104)
 
-    private lazy var medalHelperView = UIView()
+    private lazy var mentorCommentsTitleLabel = UILabel()
+        .setting(\.text, to: "Comentários do Mentor")
+        .setting(\.font, to: .mediumTitleBold)
+        .setting(\.textColor, to: .white)
+        .setting(\.textAlignment, to: .center)
 
-    private lazy var mentorCommentsStackView = VStackView([])
-
-    private lazy var mentorCommentsTitleHelperView = UIView()
-
-    private lazy var mentorCommentsTitleLabel: UILabel = {
-        let label = UILabel()
-        label.text = "Comentários do Mentor"
-        label.font = .mediumBodyBold
-        label.textColor = .white
-        label.translatesAutoresizingMaskIntoConstraints = false
-        label.accessibilityIdentifier = "StudentProjectRankingCell.mentorCommentsStackView"
-
-        return label
-    }()
-
-    private lazy var mentorCommentsDescriptionLabel: UILabel = {
-        let label = UILabel()
-        label.text = """
-Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-Nulla bibendum elit tellus, at condimentum mauris sagittis ut. Nam auctor nunc non ipsum blandit tempus.
-"""
-        label.font = .smallBody
-        label.textColor = .white
-        label.numberOfLines = 4
-        label.translatesAutoresizingMaskIntoConstraints = false
-        label.accessibilityIdentifier = "StudentProjectRankingCell.mentorCommentsStackView"
-
-        return label
-    }()
-
-    private lazy var mentorCommentsDescriptionHelperView: UIView = {
-        let view = UIView()
-        view.translatesAutoresizingMaskIntoConstraints = false
-        view.accessibilityIdentifier = "StudentProjectRankingCell.mentorCommentsDescriptionHelperView"
-
-        return view
-    }()
+    private lazy var mentorCommentsDescriptionLabel = UILabel()
+        .setting(\.text, to: Constants.defaultTexts.lorem)
+        .setting(\.font, to: .smallBody)
+        .setting(\.textColor, to: .white)
+        .setting(\.numberOfLines, to: 4)
+        .padding([.leading, .trailing], 9)
 
     // MARK: - Initializer
-
     override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
         super.init(style: style, reuseIdentifier: reuseIdentifier)
-
     }
 
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
+}
 
-    // MARK: - Subviews
-
-    func configureCell(using project: ProjectModel) {
-        self.projectTitleLabel.text = project.name
-        mentorCommentsDescriptionLabel.text = project.scoreDescription
-        configureSubviews(with: project)
-        setupConstraints()
+// MARK: - View Codable
+extension StudentProjectRankingCell: ViewCodable {
+    func buildHierarchy() {
+        addSubview(mainStackView)
     }
 
-    private func configureSubviews(with project: ProjectModel) {
+    func setupConstraints() {
+        mainStackView.edges()
+    }
+
+    func applyAdditionalChanges() {
         self.backgroundColor = .clear
+    }
+}
 
-        addSubview(mainStackView)
+// MARK: - Additional Methods
+extension StudentProjectRankingCell {
+    func configureCell(using project: ProjectModel) {
+        self.projectTitleLabel.text = project.name
+        mentorCommentsDescriptionLabel.originalView.text = project.scoreDescription
 
-        placementHelperView.addSubview(placementImageView)
-
-        medalHelperView.addSubview(medalImageView)
-
-        mentorCommentsStackView.addArrangedSubview(mentorCommentsTitleHelperView)
-
-        mentorCommentsTitleHelperView.addSubview(mentorCommentsTitleLabel)
-
-        mentorCommentsStackView.addArrangedSubview(mentorCommentsDescriptionHelperView)
-
-        mentorCommentsDescriptionHelperView.addSubview(mentorCommentsDescriptionLabel)
-
+        setupView()
         setPlacementImages(with: project)
     }
 
@@ -135,85 +115,47 @@ Nulla bibendum elit tellus, at condimentum mauris sagittis ut. Nam auctor nunc n
         }
 
         if projectPlacement < 6 {
-            switch project.placement {
-            case 1:
-                placementImageView.image = .pharusImages.firstPlaceImage
-                medalImageView.image = .pharusImages.firstPlaceMedalImage
-            case 2:
-                placementImageView.image = .pharusImages.secondPlaceImage
-                medalImageView.image = .pharusImages.secondPlaceMedalImage
-            case 3:
-                placementImageView.image = .pharusImages.thirdPlaceImage
-                medalImageView.image = .pharusImages.thirdPlaceMedalImage
-            case 4:
-                placementImageView.image = .pharusImages.fourthPlaceImage
-                medalImageView.image = .pharusImages.fourthPlaceMedalImage
-            default:
-                placementImageView.image = .pharusImages.fifthPlaceImage
-                medalImageView.image = .pharusImages.fifthPlaceMedalImage
-            }
-
-            // Placement Image View
-            placementImageView.center(in: placementHelperView)
-            NSLayoutConstraint.activate([
-                placementImageView.heightAnchor.constraint(equalToConstant: 102),
-                placementImageView.widthAnchor.constraint(equalTo: placementHelperView.widthAnchor)
-            ])
-
-            // Medal Image View
-            medalImageView.center(in: medalHelperView)
-            NSLayoutConstraint.activate([
-                medalImageView.heightAnchor.constraint(equalToConstant: 104),
-                medalImageView.widthAnchor.constraint(equalToConstant: 104)
-            ])
-
+            setOnPodiumView(with: projectPlacement)
         } else {
-            placementImageView.removeFromSuperview()
-            medalImageView.removeFromSuperview()
-            congratulationsLabel.text = "Parabéns! Você completou o projeto e sua colocação foi:"
-
-            placementStackView.axis = .vertical
-            placementStackView.removeFullyAllArrangedSubviews()
-
-            let notOnPodiumLabel = UILabel()
-            notOnPodiumLabel.font = .largeTitleBold
-            notOnPodiumLabel.textColor = .white
-            notOnPodiumLabel.text = "\(project.placement ?? 6)º Lugar"
-            notOnPodiumLabel.textAlignment = .center
-
-            placementStackView.addArrangedSubview(notOnPodiumLabel)
-            mentorCommentsTitleLabel.font = UIFont.mediumBody
+            setNotOnPodiumView(with: projectPlacement)
         }
     }
 
-    // MARK: - Constraints
+    private func setOnPodiumView(with placement: Int) {
+        switch placement {
+        case 1:
+            placementImageView.image = .pharusImages.firstPlaceImage
+            medalImageView.image = .pharusImages.firstPlaceMedalImage
+        case 2:
+            placementImageView.image = .pharusImages.secondPlaceImage
+            medalImageView.image = .pharusImages.secondPlaceMedalImage
+        case 3:
+            placementImageView.image = .pharusImages.thirdPlaceImage
+            medalImageView.image = .pharusImages.thirdPlaceMedalImage
+        case 4:
+            placementImageView.image = .pharusImages.fourthPlaceImage
+            medalImageView.image = .pharusImages.fourthPlaceMedalImage
+        default:
+            placementImageView.image = .pharusImages.fifthPlaceImage
+            medalImageView.image = .pharusImages.fifthPlaceMedalImage
+        }
+    }
 
-    private func setupConstraints() {
-        mainStackView.edges()
+    private func setNotOnPodiumView(with placement: Int) {
+        placementImageView.removeFromSuperview()
+        medalImageView.removeFromSuperview()
+        congratulationsLabel.text = "Parabéns! Você completou o projeto e sua colocação foi:"
 
-        // Mentor Comments Title Helper View
-        NSLayoutConstraint.activate([
-            mentorCommentsTitleHelperView.heightAnchor.constraint(equalToConstant: 54)
-        ])
+        placementStackView.axis = .vertical
+        placementStackView.removeFullyAllArrangedSubviews()
+        placementStackView.addArrangedSubview(
+            UILabel()
+                .setting(\.font, to: .largeTitleBold)
+                .setting(\.textColor, to: .white)
+                .setting(\.text, to: "\(placement)º Lugar")
+                .setting(\.textAlignment, to: .center)
+        )
 
-        // Mentor Comments Title Label
-        mentorCommentsTitleLabel.center(in: mentorCommentsTitleHelperView)
-
-        // Mentor Comment Description Helper View
-        NSLayoutConstraint.activate([
-            mentorCommentsDescriptionHelperView.heightAnchor.constraint(equalToConstant: 80)
-        ])
-
-        // Mentor Comments Description Label
-        NSLayoutConstraint.activate([
-            mentorCommentsDescriptionLabel.leadingAnchor.constraint(
-                equalTo: mentorCommentsTitleHelperView.leadingAnchor,
-                constant: 9
-            ),
-            mentorCommentsDescriptionLabel.trailingAnchor.constraint(
-                equalTo: mentorCommentsTitleHelperView.trailingAnchor,
-                constant: -9
-            )
-        ])
+        mentorCommentsTitleLabel.font = UIFont.mediumBody
     }
 }
