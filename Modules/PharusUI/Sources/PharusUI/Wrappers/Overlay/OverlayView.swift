24a667b8cea7614e51b4de382cfbdf0c5f05a0ba
VIctor <colen.victor@icloud.com>
2022-08-05 11:26:25 -0300

    refactor: OverlayWrapper with alignment

--- Modules/PharusUI/Sources/PharusUI/Wrappers/Overlay/OverlayView.swift
+++ Modules/PharusUI/Sources/PharusUI/Wrappers/Overlay/OverlayView.swift
@@ -12,15 +12,18 @@ public class OverlayView<Original: UIView, Wrapped: UIView, Overlay: UIView>: UI
     public let originalView: Original
     public let wrappedView: Wrapped
     public let overlay: Overlay
+    public let alignment: OverlayAlignment?
 
     init(
         original: Original,
         wrapped: Wrapped,
-        overlay: Overlay
+        overlay: Overlay,
+        alignment: OverlayAlignment? = nil
     ) {
         self.originalView = original
         self.wrappedView = wrapped
         self.overlay = overlay
+        self.alignment = alignment
 
         super.init(frame: .zero)
 
@@ -41,7 +44,54 @@ extension OverlayView: ViewCodable {
     public func setupConstraints() {
         wrappedView.translatesAutoresizingMaskIntoConstraints = false
         overlay.translatesAutoresizingMaskIntoConstraints = false
+
+        if let alignment = alignment {
+            switch alignment {
+            case .top:
+                NSLayoutConstraint.activate([
+                    overlay.topAnchor.constraint(equalTo: topAnchor),
+                    overlay.centerXAnchor.constraint(equalTo: centerXAnchor)
+                ])
+            case .bottom:
+                NSLayoutConstraint.activate([
+                    overlay.bottomAnchor.constraint(equalTo: bottomAnchor),
+                    overlay.centerXAnchor.constraint(equalTo: centerXAnchor)
+                ])
+            case .leading:
+                NSLayoutConstraint.activate([
+                    overlay.leadingAnchor.constraint(equalTo: leadingAnchor),
+                    overlay.centerYAnchor.constraint(equalTo: centerYAnchor)
+                ])
+            case .trailing:
+                NSLayoutConstraint.activate([
+                    overlay.trailingAnchor.constraint(equalTo: trailingAnchor),
+                    overlay.centerYAnchor.constraint(equalTo: centerYAnchor)
+                ])
+            case .topLeading:
+                NSLayoutConstraint.activate([
+                    overlay.topAnchor.constraint(equalTo: topAnchor),
+                    overlay.leadingAnchor.constraint(equalTo: leadingAnchor)
+                ])
+            case .topTrailing:
+                NSLayoutConstraint.activate([
+                    overlay.topAnchor.constraint(equalTo: topAnchor),
+                    overlay.trailingAnchor.constraint(equalTo: trailingAnchor)
+                ])
+            case .bottomLeading:
+                NSLayoutConstraint.activate([
+                    overlay.bottomAnchor.constraint(equalTo: bottomAnchor),
+                    overlay.leadingAnchor.constraint(equalTo: leadingAnchor)
+                ])
+            case .bottomTrailing:
+                NSLayoutConstraint.activate([
+                    overlay.bottomAnchor.constraint(equalTo: bottomAnchor),
+                    overlay.trailingAnchor.constraint(equalTo: trailingAnchor)
+                ])
+            }
+        } else {
+            overlay.edges()
+        }
+
         wrappedView.edges()
-        overlay.edges()
     }
 }
