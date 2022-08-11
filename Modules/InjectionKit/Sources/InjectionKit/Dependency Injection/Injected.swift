0d90aeca76ccee96d1d961d7fb52f0f2f2d859c5
VIctor <colen.victor@icloud.com>
2022-08-11 14:51:34 -0300

    add injectionkit

---
+++ Modules/InjectionKit/Sources/InjectionKit/Dependency Injection/Injected.swift
@@ -0,0 +1,18 @@
+//
+//  Injected.swift
+//  
+//
+//  Created by Victor Colen on 11/08/22.
+//
+
+import Foundation
+
+@propertyWrapper
+public struct Injected<T> {
+    
+    public let wrappedValue: T
+    
+    public init() {
+        wrappedValue = Container.shared.resolve()
+    }
+}
---
+++ Modules/InjectionKit/Sources/InjectionKit/InjectionKit.swift
@@ -0,0 +1,6 @@
+public struct InjectionKit {
+    public private(set) var text = "Hello, World!"
+
+    public init() {
+    }
+}
